98fd236437996ff563e5a53e8a3251eaa408099d
Alyson Takai <shtakai@gmail.com>
2018-12-23 17:17:36 +0700

    Add validation/ callback

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,12 @@
 class Link < ApplicationRecord
+  before_create :generate_short_id
+
+  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/
+  validates :url, uniqueness: true
+
+  private
+
+  def generate_short_id
+    self.short_id = Array.new(10) { Faker::Types.character }.join
+  end
 end
