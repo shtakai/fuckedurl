9bd1d5209c432538806cc6481dc19c76544d532d
Alyson Takai <shtakai@gmail.com>
2018-12-25 20:26:02 +0700

    Add more specs on link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,5 +3,7 @@
 class LinksController < ApplicationController
   def index
     @link_count = Link.count
+    # TODO magic number, pagination
+    @links = Link.last(10)
   end
 end
--- spec/system/links_spec.rb
+++ spec/system/links_spec.rb
@@ -8,9 +8,23 @@ describe 'Links', type: :system, js: true do
       2.times { create(:link, :valid_url) }
     end
 
-    it 'responds successfully' do
+    it 'displays "2" number of links' do
       visit links_path
       expect(page).to have_content 'there are 2 fucked up link(s).'
     end
+
+    it 'shows 2 links' do
+      visit links_path
+      expect(page).to have_selector('.link>a.link_url', count: 2)
+    end
   end
+
+  # NOTE: VCR can't record system spec's transitions.
+  # So that it must to write spec on request spec.
+  # walkaround: use puffying-billy
+  #
+  # describe 'click link url' do
+  #   it 'redirect to somewhere' do
+  #   end
+  # end
 end
