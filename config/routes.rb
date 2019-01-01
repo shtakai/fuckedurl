5697f473ac99226a7ac222f4b9732cb4c52e4ff0
Alyson Takai <shtakai@gmail.com>
2019-01-01 08:16:20 +0700

    Modify root page's spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  root 'links#index'
   resources :links, only: %i(index create)
   get '/redirect/:short_id', to: 'redirect#redirect', as: :redirect
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
